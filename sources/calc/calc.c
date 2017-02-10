7596c006be5bcbc231af449e3245b00b1a185cec
Lionel GATIBELZA <lgatibel@e1r1p6.42.fr>
2017-02-10 15:58:34 +0100

    add calc.c

--- sources/calc/calc.c
+++ sources/calc/calc.c
@@ -1,38 +1,64 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   calc.c                                             :+:      :+:    :+:   */
+/*   calc.cl                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgatibel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/09 17:50:51 by lgatibel          #+#    #+#             */
-/*   Updated: 2017/02/09 18:06:44 by lgatibel         ###   ########.fr       */
+/*   Updated: 2017/02/10 15:58:09 by lgatibel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <structure.h>
-//#include <lionel.h>
 
-t_lionel			calc(t_scene *scene, t_ray *ray)
+
+float	object_dist(global t_obj *objs, float3 ray_pos, float3 ray_dir)
 {
-	short	index;
-	t_obj	*obj;
+	if (objs->type == PLANE)
+		return (calc_sphere_dist(objs, ray_pos, ray_dir));
+	else if (objs->type == CONE)
+		return (calc_cone_dist(objs, ray_pos, ray_dir));
+	else if (objs->type == CYLINDER)
+		return (calc_cylinder_dist(objs, ray_pos, ray_dir));
+	else if (objs->type == SPHERE)
+		return (calc_sphere_dist(objs, ray_pos, ray_dir));
+	return (-1);
+}
 
+void	touch_object(t_obj *objs, short nobjs, float3 ray_pos, float3 ray_dir, short *index)
+{
+	short	i;
+	short	index;
+	float	small_dist;
+	float	tmp;
+
+	i = -1;
 	index = -1;
-	obj = scene->g_list;
-	while (index++ < scene->n_objs)
+	small_dist = -1;
+	while(i++ <  nobjs)
 	{
-		obj = scene->g_list;
-		if (obj->type == PLANE)
-			calc_plane();
-		else if (obj->type == CONE)
-			calc_cone();
-		else if (obj->type == CYLINDER)
-			calc_cylinder();
-		else if (obj->type == SPHERE)
-			calc_sphere();
-		if ()
+		tmp = object_dist(objs, ray_pos, ray_dir);
+		if (tmp > 0 && (tmp < small_dist || small_dist < 0))
 		{
+			index = i;
+			closest
 		}
+		objs = objs->next;
 	}
+	*dist = small_dist;
+	return (index);
+}
+
+void calc(global t_obj *objs, short nobjs, global t_obj *lgts, short nblgts, float3 ray_pos)
+{
+    short	index;
+    float3	intesection;
+    float3	dist;
+
+	dist = -1
+    index = calc_obj_dist(objs, nobjs, ray_pos, ray_dir, &dist);
+
+    intersetion = ray * dist;
+
+    get_lighting(objs, lgts, nobjs, nlgts, ambiant, ray_pos, ray_dir, index);
 }
