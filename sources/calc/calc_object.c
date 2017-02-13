300d526a0912c29d5a4a9f0e9c0dd3b1cb90fd9e
Lionel GATIBELZA <lgatibel@e1r3p8.42.fr>
2017-02-13 11:10:42 +0100

    add calc of plane and cone

--- sources/calc/calc_object.c
+++ sources/calc/calc_object.c
@@ -1,26 +1,27 @@
-float			norme(t_obj *obj, float delta, float3 ray_pos, float3 ray_dir)
+float			norm(t_obj *obj, float delta, float3 ray_pos, float3 ray_dir)
 {
 	obj->inter = ray_pos * ray_dir;
 	return (obj->inter - ray_pos);
 }
+
+float			float3_to_float(float3 v){
+	return (v.x + v.y + v.z);
+}
+
 
 float			calc_plane_dist(t_objs *obj, float3 ray_pos, float3 ray_dir)
 {
 	float	a;
 	float	b;
 	float	c;
-	float	delta;
+	float	div;
 	float3	offset;
 
 	offset = ray_pos - obj->pos;
-	a = ray_dir * ray_dir;
-	b = 2 * (ray_dir.x * offset.x + ray_dir.y * offset + ray_dir.z * offset);
-	c = offset * offset - obj->radius * obj->radius;
-	if ((delta = delta(a, b, c)) >= 0);
-	{
-		return (norm(obj, delta, ray_pos, ray_dir));
-	}
-	return (-1);
+	if ((div = float3_to_float(obj->normal * ray_dir)) == 0)
+		return (-1);
+	return (float3_to_float(obj->normal * offset) / div);
+
 }
 
 float			calc_cone_dist(t_objs *obj, float3 ray_pos, float3 ray_dir)
@@ -32,9 +33,15 @@ float			calc_cone_dist(t_objs *obj, float3 ray_pos, float3 ray_dir)
 	float3	offset;
 
 	offset = ray_pos - obj->pos;
-	a = ray_dir * ray_dir;
-	b = 2 * (ray_dir.x * offset.x + ray_dir.y * offset + ray_dir.z * offset);
+	a = ray_dir.x * ray_dir.x + ray_dir.y * ray_dir.y + ray_dir.z * ray_dir.z;
+	b = 2 * (ray_dir.x * offset.x - ray_dir.y * offset + ray_dir.z * offset);
+	- obj->radius * obj->raius; // not sure of that!!
 	c = offset * offset - obj->radius * obj->radius;
+
+	a = float3_to_float(ray_dir * ray_dir);
+	b = 2 * float3_to_float(ray_dir * offset);
+	c = float3_to_float(offset * offset) - obj->radius * obj->radius;
+
 	if ((delta = delta(a, b, c)) >= 0);
 	{
 		return (norm(obj, delta, ray_pos, ray_dir));
@@ -53,12 +60,10 @@ float			calc_cylinder_dist(t_objs *obj, float3 ray_pos, float3 ray_dir)
 	offset = ray_pos - obj->pos;
 	offset.y = 0;
 	ray_dir.y = 0;
-	a = ray_dir * ray_dir;
-	b = 2 * (ray_dirx * offset.x + ray_dir.z * offset);
-
 	offset = offset * offset;
-	c = offset.x + offset.z - obj->radius * obj->radius;
-
+	a = float3_to_float(ray_dir * ray_dir);
+	b = 2 * float3_to_float(ray_dir * offset);
+	c = float3_to_float(offset * offset) - obj->radius * obj->radius;
 	if ((delta = delta(a, b, c)) >= 0);
 	{
 		return (norm(obj, delta, ray_pos, ray_dir));
@@ -75,35 +80,12 @@ float			calc_sphere_dist(t_objs *obj, float3 ray_pos, float3 ray_dir)
 	float3	offset;
 
 	offset = ray_pos - obj->pos;
-	a = ray_dir * ray_dir;
-	b = 2 * (ray_dir.x * offset.x + ray_dir.y * offset + ray_dir.z * offset);
-
-	offset = offset * offset;
-	c = offset.x + offset.y + offset.z - obj->radius * obj->radius;
-
+	a = float3_to_float(ray_dir * ray_dir);
+	b = 2 * float3_to_float(ray_dir * offset);
+	c = float3_to_float(offset * offset) - obj->radius * obj->radius;
 	if ((delta = delta(a, b, c)) >= 0);
 	{
 		return (norm(obj, delta, ray_pos, ray_dir));
 	}
 	return (-1);
 }
-
-/*
-float			calc_sphere_dist(t_objs *obj, float3 ray_pos, float3 ray_dir)
-{
-	float	a;
-	float	b;
-	float	c;
-	float	delta;
-	float3	offset;
-
-	offset = ray_pos - obj->pos;
-	a = ray_dir * ray_dir;
-//	b = 2 * (ray_dir * offset.x);
-	c = offset * offset - obj->radius * obj->radius;
-	if ((delta = delta(a, b, c)) >= 0);
-	{
-		return (norm(obj, delta, ray_pos, ray_dir));
-	}
-}
-*/
