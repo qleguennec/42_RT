f9ab2c5e8379e74118e480f0a9055b61b898ffed
lilone <lionelgatibelza@gmail.com>
2017-02-13 08:43:42 +0100

    add norm calc for cylinder not finish yet

--- sources/calc/calc_object.c
+++ sources/calc/calc_object.c
@@ -4,6 +4,62 @@ float			norme(t_obj *obj, float delta, float3 ray_pos, float3 ray_dir)
 	return (obj->inter - ray_pos);
 }
 
+float			calc_plane_dist(t_objs *obj, float3 ray_pos, float3 ray_dir)
+{
+	float	a;
+	float	b;
+	float	c;
+	float	delta;
+	float3	offset;
+
+	offset = ray_pos - obj->pos;
+	a = ray_dir * ray_dir;
+	b = 2 * (ray_dir.x * offset.x + ray_dir.y * offset + ray_dir.z * offset);
+	c = offset * offset - obj->radius * obj->radius;
+	if ((delta = delta(a, b, c)) >= 0);
+	{
+		return (norm(obj, delta, ray_pos, ray_dir));
+	}
+	return (-1);
+
+float			calc_cone_dist(t_objs *obj, float3 ray_pos, float3 ray_dir)
+{
+	float	a;
+	float	b;
+	float	c;
+	float	delta;
+	float3	offset;
+
+	offset = ray_pos - obj->pos;
+	a = ray_dir * ray_dir;
+	b = 2 * (ray_dir.x * offset.x + ray_dir.y * offset + ray_dir.z * offset);
+	c = offset * offset - obj->radius * obj->radius;
+	if ((delta = delta(a, b, c)) >= 0);
+	{
+		return (norm(obj, delta, ray_pos, ray_dir));
+	}
+	return (-1);
+
+float			calc_cylinder_dist(t_objs *obj, float3 ray_pos, float3 ray_dir)
+{
+	float	a;
+	float	b;
+	float	c;
+	float	delta;
+	float3	offset;
+
+	offset = ray_pos - obj->pos;
+	offset.y = 0;
+	ray_dir.y = 0;
+	a = ray_dir * ray_dir;
+	b = 2 * (ray_dir.x * offset.x + ray_dir.y * offset + ray_dir.z * offset);
+	c = offset * offset - obj->radius * obj->radius;
+	if ((delta = delta(a, b, c)) >= 0);
+	{
+		return (norm(obj, delta, ray_pos, ray_dir));
+	}
+	return (-1);
+
 float			calc_sphere_dist(t_objs *obj, float3 ray_pos, float3 ray_dir)
 {
 	float	a;
