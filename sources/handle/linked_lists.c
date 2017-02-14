4fa5e44c870a1a49fcf5320d2c896f3b686cc254
Benjamin SOUCHET <bsouchet@e1r2p4.42.fr>
2017-02-14 09:28:50 +0100

    Edit linked list

--- sources/handle/linked_lists.c
+++ sources/handle/linked_lists.c
@@ -6,7 +6,7 @@
 /*   By: bsouchet <bsouchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 10:41:16 by bsouchet          #+#    #+#             */
-/*   Updated: 2017/02/11 18:26:11 by bsouchet         ###   ########.fr       */
+/*   Updated: 2017/02/14 09:22:32 by bsouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,9 @@ t_obj		*lst_new_camera(t_rt *rt, t_obj *objs, int title)
 		return (NULL);
 	new_cam = set_element_parameters(new_cam, rt->prs->obj_tmp, 'C', title);
 	new_cam->next = NULL;
-	if (objs == NULL && ++rt->scn->n_cams)
+	if (objs == NULL)
 	{
 		rt->scn->b_lgts = new_cam->next;
-		rt->scn->n_elms++;
 		new_cam->id = rt->ui->id++;
 		return (new_cam);
 	}
@@ -35,8 +34,8 @@ t_obj		*lst_new_camera(t_rt *rt, t_obj *objs, int title)
 	new_cam->next = nav_list->next;
 	nav_list->next = new_cam;
 	rt->scn->b_lgts = new_cam->next;
-	rt->scn->n_cams++;
-	rt->scn->n_elms++;
+	rt->scn->n_cams += (title == 0) ? 1 : 0;
+	rt->scn->n_elms += (title == 0) ? 1 : 0;
 	new_cam->id = rt->ui->id++;
 	return (objs);
 }
@@ -51,21 +50,15 @@ t_obj		*lst_new_light(t_rt *rt, t_obj *objs, int title)
 		return (NULL);
 	new_light = set_element_parameters(new_light, rt->prs->obj_tmp, 'L', title);
 	new_light->next = NULL;
-	if (objs == NULL && ++rt->scn->n_lgts)
-	{
-		rt->scn->b_objs = new_light->next;
-		rt->scn->n_elms++;
-		new_light->id = rt->ui->id++;
-		return (new_light);
-	}
 	nav_list = objs;
-	while (nav_list->next != NULL && nav_list->next->type < 79)
+	while (nav_list->next != NULL && 
+	(nav_list->next->type == 'C' || nav_list->next->type == 'L'))
 		nav_list = nav_list->next;
 	new_light->next = nav_list->next;
 	nav_list->next = new_light;
 	rt->scn->b_objs = new_light->next;
-	rt->scn->n_lgts++;
-	rt->scn->n_elms++;
+	rt->scn->n_lgts += (title == 0) ? 1 : 0;
+	rt->scn->n_elms += (title == 0) ? 1 : 0;
 	new_light->id = rt->ui->id++;
 	return (objs);
 }
@@ -81,18 +74,12 @@ t_obj		*lst_new_object(t_rt *rt, t_obj *objs, int title, int type)
 		return (NULL);
 	new_obj = set_element_parameters(new_obj, rt->prs->obj_tmp, 'O', title);
 	new_obj->next = NULL;
-	if (objs == NULL && ++rt->scn->n_objs)
-	{
-		rt->scn->n_elms++;
-		new_obj->id = rt->ui->id++;
-		return (new_obj);
-	}
 	nav_list = objs;
 	while (nav_list->next != NULL)
 		nav_list = nav_list->next;
 	nav_list->next = new_obj;
-	rt->scn->n_objs++;
-	rt->scn->n_elms++;
+	rt->scn->n_objs += (title == 0) ? 1 : 0;
+	rt->scn->n_elms += (title == 0) ? 1 : 0;
 	new_obj->id = rt->ui->id++;
 	return (objs);
 }
