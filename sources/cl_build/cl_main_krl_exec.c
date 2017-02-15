0450356e4c22b8d6a50bce3070efb3fdfd0feac7
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2017-02-15 17:07:40 +0000

    fix krl_exec to skip update of buffers

--- sources/cl_build/cl_main_krl_exec.c
+++ sources/cl_build/cl_main_krl_exec.c
@@ -6,25 +6,27 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/08 12:07:51 by qle-guen          #+#    #+#             */
-/*   Updated: 2017/02/14 15:19:40 by qle-guen         ###   ########.fr       */
+/*   Updated: 2017/02/15 17:07:31 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 #include "libfmt.h"
 
+// TODO remove debug includes
+#include <assert.h>
+
 bool
 	cl_main_krl_exec
 	(t_cl *cl
 	, t_scene *scn)
+// TODO remove scn ptr from parameters
 {
 	int			ret;
 	size_t		work_size[2];
 
-	if (!cl_main_krl_update_camera(cl, scn->c_cam))
-		return (ERR("cannot update camera", false, 0));
-	if (!cl_main_krl_update_buffers(cl, scn))
-		return (ERR("cannot update buffers", false, 0));
+	assert(cl->n_lgts == scn->n_lgts);
+	assert(cl->n_objs == scn->n_objs);
 	work_size[0] = WIN_W;
 	work_size[1] = WIN_H;
 	if ((ret = cl_krl_exec(&cl->info, cl->main_krl.krl, 2, work_size))
